28b2a7ea383f50a6550305616127fbb59d879f22
java-saeng <gusdn85554@naver.com>
2025-03-25 01:59:20 +0900

    initial commit

---
+++ src/main/java/com/practice/self_archive/SelfArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.practice.self_archive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfArchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/self_archive/SelfArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.self_archive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
